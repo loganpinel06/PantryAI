75413ed278d37435519fcb76949756eb63dc8016
Logan Pinel <loganpinel@outlook.com>
2025-07-27 16:32:16 -0700

    FIXED: forgot to add current user id when querying and posting new data

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@
 #imports
 from flask import Blueprint, render_template, redirect, url_for, jsonify
 #import Flask-Login for user authentication
-from flask_login import login_required
+from flask_login import login_required, current_user
 #import the Pantry model
 from .models import Pantry
 #import the db object from __init__.py to connect to the database
@@ -20,8 +20,8 @@ view = Blueprint('view', __name__)
 def pantry():
     #creat the PantryForm instance
     pantry_form = PantryForm()
-    #query the Pantry model to get all pantry items
-    pantry_items = Pantry.query.all()
+    #query the Pantry model to get all pantry items for the current user
+    pantry_items = Pantry.query.filter_by(user_id=current_user.id).all()
     #render the pantry.html template with the pantry items
     return render_template('pantry.html', pantry_items=pantry_items, form=pantry_form)
 
@@ -36,8 +36,8 @@ def add_ingredient():
     if pantry_form.validate_on_submit():
         #get the data from the form
         ingredient = pantry_form.ingredient.data
-        #create a new Pantry object
-        new_pantry_object = Pantry(ingredient=ingredient)
+        #create a new Pantry object with the current user's id
+        new_pantry_object = Pantry(ingredient=ingredient, user_id=current_user.id)
         #try, except block to add the Pantry object to the database
         try: 
             #connect to the db and add the pantry object
@@ -68,8 +68,8 @@ def add_ingredient():
 @view.route('/api/pantry/delete-ingredient/<int:id>', methods=['DELETE'])
 @login_required  #require user to be logged in to access this route
 def delete_ingredient(id:int):
-    #query the Pantry model to get the pantry item by id
-    pantry_item_to_delete = Pantry.query.get_or_404(id)
+    #query the Pantry model to get the pantry item by id and ensure it belongs to the current user
+    pantry_item_to_delete = Pantry.query.filter_by(id=id, user_id=current_user.id).first_or_404()
     #try, except block to delete the pantry item from the database and handle errors
     try:
         #connect to the db and delete the pantry item
