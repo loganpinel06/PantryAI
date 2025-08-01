f4ca2adada6dccbef2105bc0faa66cf50a1c11d7
Logan Pinel <loganpinel@outlook.com>
2025-07-31 17:25:29 -0700

    Configured the Gemini API to work with JSON data which will make using the response on the frontend easier and also added a docstring for readability and understanding of the model

--- app/gemini.py
+++ app/gemini.py
@@ -3,23 +3,48 @@
 #imports
 from google import genai
 from google.genai import types
-from . import genai_client
+from pydantic import BaseModel
+from . import gemini_client
+
+#create a pydantic BaseModel for the recipe request
+#this will be used to help format Gemini's response to JSON data for easier handling on the frontend
+class Recipe(BaseModel):
+    recipe_name: str
+    ingredients: list[str]
+    instructions: str
 
 #create a method to prompt the Gemini API for a recipe
 def generate_recipe(ingredients_list, meal_type):
-    #create a prompt for the Gemini API
-    prompt = f"Please generate two quality recipes for {meal_type} using the following list of ingredients: {', '.join(ingredients_list)}"
+    """
+    Generate recipes using the Gemini API based on provided ingredients and meal type.
+    
+    Args:
+        ingredients_list (list[str]): List of available ingredients
+        meal_type (str): Type of meal (e.g., 'breakfast', 'lunch', 'dinner')
+    
+    Returns:
+        Response object from Gemini API containing recipe data, or an error message if error occurs
+    """
+    #try, except block to handle errors when generating a recipe
+    try:
+        #create a prompt for the Gemini API
+        prompt = f"Please generate two quality recipes for {meal_type} using the following list of ingredients: {', '.join(ingredients_list)}"
 
-    #send the prompt to the Gemini API
-    response = genai_client.models.generate_content(
-        model="gemini-2.5-flash-lite",
-        contents=prompt,
-        #Disable thinking (CODE FROM GEMINI API DOCS)
-        #this will reduce the response time and token usage/costs
-        config=types.GenerateContentConfig(
-        thinking_config=types.ThinkingConfig(thinking_budget=0)
-        ),
-    )
+        #send the prompt to the Gemini API
+        response = gemini_client.models.generate_content(
+            model="gemini-2.5-flash-lite",
+            contents=prompt,
+            #Configure response format to json using the Recipe BaseModel schema and disable thinking
+            config=types.GenerateContentConfig(
+                response_mime_type="application/json",
+                response_schema=list[Recipe],
+                thinking_config=types.ThinkingConfig(thinking_budget=0)
+            ),
+        )
+    #ERROR
+    except Exception as e:
+        #return the error message (CHANGE THIS FROM PRINT LATER)
+        return f"Error generating recipe: {e}"
 
     #return the response text
     return response
